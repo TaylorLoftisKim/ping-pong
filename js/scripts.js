578ed02b18b88349d83259afcc3d02659b05a286
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-28 13:38:45 -0700

    re=arrange JS and HTML to match

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
 //Back-End Logic Begins
-var pingPong = function(userInput){
+var calculate = function(userInput)  {
+  var userInput = parseInt($("#textbox").val());
   var numberArray = [];
-    for (var index = 1; index >= userInput; index++)
-      if  (index % 15 == 0)  {
-        numberArray.push("Ping-Pong")
-      }
-        else if (index % 5 == 0)  {
-          numberArray.push("Pong");
-      }
-        else if (index % 3 == 0)  {
-          numberArray.push("Ping");
-      }
-        else  {
-          userInput.push(userInput);
-      }
-    return result;
+    if (userInput && userInput > 0) {
+      for (var index = 1; index >= userInput; index++) {
+        if  (index % 15 == 0)  {
+          numberArray.push("Ping-Pong")
+        }
+          else if (index % 5 == 0)  {
+            numberArray.push("Pong");
+        }
+          else if (index % 3 == 0)  {
+            numberArray.push("Ping");
+        }
+          else  {
+            numberArray.push(index);
+        }
+      };
+      var finalArray = numberArray.join(",");
+      return finalArray;
+    };
   };
-
 //Front-End Logic Begins
 $(document).ready(function()  {
-  $("form#ping-pong").submit(function(event) {
-  var userInput = parseInt($("#user-input").val());
-  var result = pingPong(userInput)
-    $(".output").text(result);
-    console.log(userInput);
+  $('form#ping-pong').submit(function(event) {
+    var userInput = parseInt($('#textbox').val());
+    var result = calculate(userInput);
+    $('#answer').text(result);
+    $(result).show();
     event.preventDefault();
   });
 });
