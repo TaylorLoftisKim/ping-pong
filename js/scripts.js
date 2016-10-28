7e17a59d9d45180531b0c297d816cec7c2d7fa7f
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-28 09:45:22 -0700

    remove un-needed js, cleaned up indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,22 @@
 //Back-End Logic Begins
-// var number = userInput.split(" ")
-// var ping = ["3","6","9","12","15","18","21","24","27","30","33","36","39","42","45","48","51","54","57","60"]
-// var pong = ["15","30","45","60"]
-// var ping-pong = ["15","30","45","60"]
-var index = 1;
 var pingPong = function(number){
   var numberArray = [];
-  for (userInput >= 0; index++)
-    if  (index % 15 === 0;)  {
+  for (var index = 1; userInput <= userInput; index++)
+  console.log(number)
+    if  (index % 15 ==  0)  {
       numberArray.push("Ping-Pong")
       }
-      else if (index % 5 === 0;)  {
+      else if (index % 5 ==  0)  {
         numberArray.push("Pong")
       }
-      else if (index % 3 === 0;)  {
+      else if (index % 3 == 0)  {
         numberArray.push("Ping")
       }
-    else return.userInput {
+    else  {
+      return userInput
       alert("Please enter a valid number!")
     }
-
-
+  }
 
 //Front-End Logic Begins
 $(document).ready(function()  {
