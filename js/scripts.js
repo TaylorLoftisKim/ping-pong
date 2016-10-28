08170db1f8e345bdddf1380003b8fb68f7ec8d0e
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-28 11:08:58 -0700

    get html and js to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 //Back-End Logic Begins
-var userInput  = ("#user-input");
-var pingPong = function(number){
-var numberArray = [];
-  for (var index = 1; index <= userInput; index++)
-    if  (index % 15 == 0)  {
-      numberArray.push("Ping-Pong");
+var pingPong = function(userInput){
+  var numberArray = [];
+    for (var index = 1; index >= userInput; index++)
+      if  (index % 15 == 0)  {
+        numberArray.push("Ping-Pong")
       }
-      else if (index % 5 == 0)  {
-        numberArray.push("Pong");
+        else if (index % 5 == 0)  {
+          numberArray.push("Pong");
       }
-      else if (index % 3 == 0)  {
-        numberArray.push("Ping");
+        else if (index % 3 == 0)  {
+          numberArray.push("Ping");
       }
-    else  {
-      return userInput.join(" ");
-    };
+        else  {
+          userInput.push(userInput);
+      }
+    return result;
   };
 
 //Front-End Logic Begins
 $(document).ready(function()  {
   $("form#ping-pong").submit(function(event) {
   var userInput = parseInt($("#user-input").val());
-    $(".output").text(pingPong(userInput));
+  var result = pingPong(userInput)
+    $(".output").text(result);
     console.log(userInput);
     event.preventDefault();
   });
