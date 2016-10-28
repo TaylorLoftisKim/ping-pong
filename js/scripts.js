f434c4272886bfb7cd8597902e8f91fad10e0b5a
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-28 10:05:19 -0700

    add proper variables in javascript, make else if statement functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,28 @@
 //Back-End Logic Begins
+var userInput  = ("#user-input");
 var pingPong = function(number){
-  var numberArray = [];
-  for (var index = 1; userInput <= userInput; index++)
-  console.log(number)
-    if  (index % 15 ==  0)  {
-      numberArray.push("Ping-Pong")
+var numberArray = [];
+  for (var index = 1; index <= userInput; index++)
+    if  (index % 15 == 0)  {
+      numberArray.push("Ping-Pong");
       }
-      else if (index % 5 ==  0)  {
-        numberArray.push("Pong")
+      else if (index % 5 == 0)  {
+        numberArray.push("Pong");
       }
       else if (index % 3 == 0)  {
-        numberArray.push("Ping")
+        numberArray.push("Ping");
       }
     else  {
-      return userInput
-      alert("Please enter a valid number!")
-    }
-  }
+      return userInput.join(" ");
+    };
+  };
 
 //Front-End Logic Begins
 $(document).ready(function()  {
   $("form#ping-pong").submit(function(event) {
-    var userInput = parseInt($("#user-input").val());
-    var output = converter(userInput);
-    console.log(output);
+  var userInput = parseInt($("#user-input").val());
+    $(".output").text(pingPong(userInput));
+    console.log(userInput);
     event.preventDefault();
   });
 });
